4a1fd72960353f867f3f28b10eb49332f125b9c9
DrGomi <omid-k@gmx.de>
2022-06-23 11:31:31 +0200

    add crop method

--- www/ImageTranscoding.js
+++ www/ImageTranscoding.js
@@ -20,3 +20,26 @@ exports.transcode = function (inImage, outImage, success, error, options) {
     );
 
 };
+
+exports.crop = function (inImage, outImage, success, error, options) {
+
+    if (!options) {
+        error('ERROR: crop method needs options-object!')
+    } else {
+        // { top: x, left: x, bottom: x, right: x, quality: x, size: x };
+        if (!options.quality) {
+            options.quality = 97;
+        }
+    
+        if (!options.size) {
+            options.size = 448;
+        }
+    
+        cordova.exec(
+            function(result) { success(result); },
+            error,
+            'ImageTranscoding',
+            'crop', [inImage, outImage, options]
+        );
+    }
+};
